ceaf5077ba245bcb38a24e29174a8d15bc71607e
masonww <mason.ww@outlook.com>
2019-07-23 09:14:03 -0700

    Update AZSubnetFunctions.ps1

--- AZSubnetFunctions.ps1
+++ AZSubnetFunctions.ps1
@@ -32,25 +32,27 @@ function Set-AZWMSubnetsEnableServiceEndpoints
     )
     Set-AzContext -Subscription $SubscriptionName
     $VNs=Get-AzVirtualNetwork
-    $subnets=$vns|Get-AzVirtualNetworkSubnetConfig
-    $list=New-Object System.Collections.Generic.List[string]
-    $list.add("Microsoft.AzureActiveDirectory")
-    $list.add("Microsoft.AzureCosmosDB")
-    $list.add("Microsoft.ContainerRegistry")
-    $list.add("Microsoft.EventHub")
-    $list.add("Microsoft.KeyVault")
-    $list.add("Microsoft.ServiceBus")
-    $list.add("Microsoft.Sql")
-    $list.add("Microsoft.Storage")
-    $list.add("Microsoft.Web")
-    $c=0
-    foreach($subnet in $Subnets)
+    foreach($VN in $VNs)
     {
-        $c++
-        Write-Progress -Activity "Adding Service Endpoints to Subnets." -Status $subnet.Name -PercentComplete ($c/$subnets.count * 100)
-        $VN = ($subnet.id).split("/")[8]
-        set-AzVirtualNetworkSubnetConfig -name $subnet.Name -VirtualNetwork $VN -AddressPrefix $subnet.AddressPrefix -ServiceEndpoint $list | Set-AzVirtualNetwork
-        
+        $subnets=$vn|Get-AzVirtualNetworkSubnetConfig
+        $list=New-Object System.Collections.Generic.List[string]
+        $list.add("Microsoft.AzureActiveDirectory")
+        $list.add("Microsoft.AzureCosmosDB")
+        $list.add("Microsoft.ContainerRegistry")
+        $list.add("Microsoft.EventHub")
+        $list.add("Microsoft.KeyVault")
+        $list.add("Microsoft.ServiceBus")
+        $list.add("Microsoft.Sql")
+        $list.add("Microsoft.Storage")
+        $list.add("Microsoft.Web")
+        $c=0
+        foreach($subnet in $Subnets)
+        {
+            $c++
+            Write-Progress -Activity "Adding Service Endpoints to Subnets." -Status $subnet.Name -PercentComplete ($c/$subnets.count * 100)
+            set-AzVirtualNetworkSubnetConfig -name $subnet.Name -VirtualNetwork $VN -AddressPrefix $subnet.AddressPrefix -ServiceEndpoint $list | Set-AzVirtualNetwork
+            $c
+        }
     }
-
+    
 }
