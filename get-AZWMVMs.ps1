4dbaebd76f61b8aaf5f00c78fd474ba5ad139ad5
masonww <mason.ww@outlook.com>
2019-08-01 15:35:10 -0700

    Add files via upload

---
+++ get-AZWMVMs.ps1
@@ -0,0 +1,22 @@
+$subscriptions = Get-AzSubscription
+$output=@()
+$c=0
+foreach($subscription in $subscriptions)
+{
+    Set-AzContext $subscription.name | out-null
+    $c++
+    write-progress -activity "Getting VMs " -Status $subscription.name -PercentComplete ($c/$subscriptions.Count*100)
+    $VMs=get-azvm -status
+    foreach($VM in $VMs)
+    {
+        $item=[PSCustomObject]@{
+            Subscription = $subscription.name
+            Name = $vm.name
+            OSType = $vm.StorageProfile.OsDisk.OsType
+            PowerState = $vm.PowerState
+
+        }
+        $output += $item
+    }    
+}
+$output|Out-GridView
